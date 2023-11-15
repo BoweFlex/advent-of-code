14fd702e56bcca63c353e2290234cf4fa5ee480a
jbowe <jbowe@webstaurantstore.com>
2023-11-15 07:11:52 -0500

    Day 1 pt2
    Updated highest_total to accept a number of elves parameter, if equal to
    1 returns the highest elf and if higher (i.e. 3) returns the sum of the
    top 3 elves.

--- 2022/python3/day1/day1.py
+++ 2022/python3/day1/day1.py
@@ -18,10 +18,12 @@ elf's inventories. For example:
 
 10000
 -----
-We'd like to take food first from the elf with carrying the most *total*
+First: We'd like to take food first from the elf with carrying the most *total*
 calories. Figure out who that is by totaling up subsequent calorie amounts.
 Solution should: read in input file, loop through lines to total each
 group of calories, then return the highest total.
+Second: We'd like to know the total calories of the top three elves *combined*.
+Find the three highest, then find the sum of those three.
 
 **MUST PROVIDE PATH TO INPUT FILE WHEN EXECUTING
 """
@@ -29,21 +31,40 @@ group of calories, then return the highest total.
 import sys
 
 
-def highest_total(input_file):
+def sum_elves(elves):
+    final_total = 0
+    for elf in elves:
+        final_total += elf
+    return final_total
+
+
+def highest_total(input_file, num_top_elves=1):
     file = open(input_file, "r")
     total = 0
-    highest_total = 0
+    top_elves = []
     # print(file.read())
     for line in file:
         if line.strip():
-            # print(line)
             total += int(line)
         else:
-            # print("This line is blank!")
-            if total > highest_total:
-                highest_total = total
+            if len(top_elves) < num_top_elves:
+                top_elves.append(total)
+            else:
+                top_elves.sort()
+                for elf in top_elves:
+                    if total > elf:
+                        top_elves.remove(elf)
+                        top_elves.append(total)
+                        break
             total = 0
-    return highest_total
+    return sum_elves(top_elves)
 
 
-print(highest_total(sys.argv[1]))
+try:
+    input_file = sys.argv[1]
+except NameError:
+    input_file = None
+
+if input_file is not None:
+    print("Highest elf:", highest_total(input_file))
+    print("Highest three elves combined:", highest_total(input_file, 3))
